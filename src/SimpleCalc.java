7ae3980c3bcdff6faf4cb28648276092bc642d98
Vivek Kumar <vivkumjsr@gmail.com>
2017-10-28 19:28:14 +0530

    Req4:Fixed code to supprt requirement 4

--- src/SimpleCalc.java
+++ src/SimpleCalc.java
@@ -2,10 +2,7 @@ public class SimpleCalc {
 	public int add(String inputString) {
 		int returnVal =0;
 		String[] numbersArray = inputString.split(",");
-		if(numbersArray.length > 2)
-		{
-			throw new RuntimeException("Max 2 numbers allowed");
-		}
+
 		for(String number: numbersArray)
 		{
 			int currentNum = Integer.parseInt(number);
