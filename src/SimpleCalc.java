cd198cd902e49e2d10e37ed3cd7fde9ed70c71e9
Vivek Kumar <vivkumjsr@gmail.com>
2017-10-28 18:58:13 +0530

    Req1: Failing Test of negative input test

---
+++ src/SimpleCalc.java
@@ -0,0 +1,6 @@
+public class SimpleCalc {
+	public int add(String inputString) {
+		
+		return 0;
+	}
+}
---
+++ src/SimpleCalcTest.java
@@ -0,0 +1,27 @@
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class SimpleCalcTest {
+	
+	private static SimpleCalc calc;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		calc = new SimpleCalc();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	
+	@Test (expected = RuntimeException.class)
+	public void whenNegativeNumberInputThenThrowException() {
+		int value = calc.add("-1,1");
+		
+	}
+
+}
