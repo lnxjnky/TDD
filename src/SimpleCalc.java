970f16770114dc1dd4b43b7a677de01b79539827
Vivek Kumar <vivkumjsr@gmail.com>
2017-10-28 19:00:10 +0530

    Req1:Fixed code to pass the failing tests

--- src/SimpleCalc.java
+++ src/SimpleCalc.java
@@ -1,6 +1,13 @@
 public class SimpleCalc {
 	public int add(String inputString) {
-		
+		String[] numbersArray = inputString.split(",");
+		for(String number: numbersArray)
+		{
+			if(Integer.parseInt(number) < 0)
+			{
+				throw new RuntimeException("Negative numbers not allowed");
+			}
+		}
 		return 0;
 	}
 }
