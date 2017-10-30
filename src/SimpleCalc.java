1bf965dbb7b57d1972171c8097cc80850e13275f
Vivek Kumar <vivkumjsr@gmail.com>
2017-10-30 20:37:31 +0530

    Req6:Refactor code to handle multiple delimter case

--- src/SimpleCalc.java
+++ src/SimpleCalc.java
@@ -1,23 +1,32 @@
 public class SimpleCalc {
 	public int add(String inputString) {
-		int returnVal =0;
-		String[] numbersArray = inputString.split(",|\n");
-
-		for(String number: numbersArray)
-		{
-			if(!number.isEmpty())
-			{
-				int currentNum = Integer.parseInt(number);
-				if(currentNum < 0)
-				{
-					throw new RuntimeException("Negative not allowed");
-				}
-				else
-				{
-					returnVal += currentNum;
-				}
-			}
-		}
-		return returnVal;
+		String delimiter = ",|\n";
+	    String numbersWithoutDelimiter = inputString;
+	    if (inputString.startsWith("//")) {
+	        int delimiterIndex = inputString.indexOf("//") + 2;
+	        delimiter = inputString.substring(delimiterIndex, delimiterIndex + 1);
+	        numbersWithoutDelimiter = inputString.substring(inputString.indexOf("\n") + 1);
+	        
+	    }
+	    return addHelper(numbersWithoutDelimiter, delimiter);
+	}
+	
+	private static int addHelper(final String numbers, final String delimiter) {
+	    int returnValue = 0;
+	    String[] numbersArray = numbers.split(delimiter);
+	    for (String number : numbersArray) {
+	        if (!number.trim().isEmpty()) {
+	        	int currentNumber = Integer.parseInt(number.trim());
+	        	if(currentNumber < 0)
+	        	{
+	                throw new RuntimeException("Negative numbers not allowed");
+	        	}
+	        	else
+	        	{
+	        		returnValue += currentNumber;
+	        	}
+	        }
+	    }
+	    return returnValue;
 	}
 }
